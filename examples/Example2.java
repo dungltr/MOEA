98f683d61642450464599ed14108e54695436e8a
Le Trung Dung <letrungdung@smith-mac-01.home>
2018-01-22 10:42:13 +0100

    good quality from laptop

--- examples/Example2.java
+++ examples/Example2.java
@@ -56,10 +56,10 @@ public class Example2 {
 	public static void main(String[] args) throws IOException {
 		List<String> Problems = new ArrayList<>();
 		String Caption = "";
-		String homeFile =ReadFile.readhome("HOME_jMetalData")+"/"+"MOEA_UF_ZDT_DTLZ_problems_using_findMax_4standard_Original";
+		String homeFile =ReadFile.readhome("HOME_jMetalData")+"/"+"MOEA_UF_ZDT_DTLZ_problems_using_findMax_4standard_NonFast_6algoritms";
 		//String texFile = File + ".tex";
 		//String[] problems = {"UF1","UF2","UF3","DTLZ1.8D","DTLZ2.8D","DTLZ3.8D"};
-		String [] allAlgorithms = { "NSGAII", "NSGAIII", "NSGAV"};//, "eMOEA", "eNSGAII", "GDE3"};//, 
+		String [] allAlgorithms = { "NSGAII", "NSGAIII", "NSGAV", "eMOEA", "eNSGAII", "GDE3"};//, 
 				//"MOEAD", "MSOPS", "CMA-ES", "SPEA2", "PAES", "PESA2", "OMOPSO",
 				//"SMPSO", "IBEA", "SMS-EMOA", "VEGA", "DBEA", "Random", "RVEA",
 				//"RSO" };
